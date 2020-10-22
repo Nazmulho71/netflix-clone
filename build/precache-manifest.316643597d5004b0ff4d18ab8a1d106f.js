eabd6d91f8dc2ad7ba73b6e48a8f76519a1312d4
Md Nazmul Hossain <72980052+Nazmulho71@users.noreply.github.com>
2020-10-22 18:23:50 +0600

    Add files via upload

---
+++ build/precache-manifest.316643597d5004b0ff4d18ab8a1d106f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a245d9900032e33564b7a54cdebf8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3054be101b4b57b3c3b9",
+    "url": "/static/css/main.7545c006.chunk.css"
+  },
+  {
+    "revision": "f25b722749daad8bffab",
+    "url": "/static/js/2.0a26a8e8.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/static/js/2.0a26a8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3054be101b4b57b3c3b9",
+    "url": "/static/js/main.050d099f.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
